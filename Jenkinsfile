f92736aeff09b41fdba96339b5e92dc156a27377
jerin viju <jerinviju.mec@gmail.com>
2020-04-26 08:01:53 +0530

    adding Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,12 @@
+stage 'CI'
+node{
+
+    checkout scm
+    try{
+      stage 'building'
+      sh 'mvn clean package'
+
+    }
+
+
+}
--- src/main/java/Subscriber.java
+++ src/main/java/Subscriber.java
@@ -1,6 +1,6 @@
 public class Subscriber {
     public static void main(String[] args) {
         System.out.println("helo");
-        
+
     }
 }
